4dae313d45a15a95d9592471f2269c185728a964
MingyeomKim <67851124+MingyeomKim@users.noreply.github.com>
2023-03-20 14:23:03 +0900

    [feature] 프로젝트 생성

---
+++ src/test/java/com/personal/iamhere/IamhereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.iamhere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IamhereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
