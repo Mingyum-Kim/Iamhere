ec73075e16645e3903cb0eaf383013c797cd7ab8
MingyeomKim <67851124+MingyeomKim@users.noreply.github.com>
2023-04-11 02:48:42 +0900

    [bugfix] spring cloud 실행 오류 해결

---
+++ src/main/java/com/personal/iamhere/IamhereApplication.java
@@ -0,0 +1,13 @@
+package com.personal.iamhere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IamhereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IamhereApplication.class, args);
+    }
+
+}
