6acb28a7c3840d5653f070ce3a87f6ba54908ac9
MingyeomKim <mingyum119@naver.com>
2023-05-03 18:22:43 +0900

    [feature] 게시판 멀티모듈 생성

---
+++ msa-iamhere-post/src/main/java/com/personal/post/MsaIamherePostApplication.java
@@ -0,0 +1,13 @@
+package com.personal.post;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaIamherePostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaIamherePostApplication.class, args);
+    }
+
+}
