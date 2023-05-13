173ed7cf6ab72134b40dff411e486800ae0acba6
MingyeomKim <67851124+MingyeomKim@users.noreply.github.com>
2023-05-13 15:42:54 +0900

    [refactor/#3] DB 암호화 로직 테스트에서 findByMail 호출제거

--- msa-iamhere-member/src/test/java/com/personal/member/service/MemberServiceTest.java
+++ msa-iamhere-member/src/test/java/com/personal/member/service/MemberServiceTest.java
@@ -7,16 +7,15 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.util.Date;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -58,6 +57,7 @@ class MemberServiceTest {
         memberDTO.setPassword(encoded);
         when(memberRepository.save(any(Member.class))).thenReturn(memberDTO.toEntity());
         when(bCryptPasswordEncoder.encode(any(String.class))).thenReturn(encoded);
+        when(memberRepository.findByMail(any(String.class))).thenReturn(Optional.empty());
 
         // when
         Member member =  memberService.insertMember(memberDTO);
