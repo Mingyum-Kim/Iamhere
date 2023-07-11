c636ac27e85a0eeb6d96366232b5c255cd06ef91
MingyeomKim <mingyum119@naver.com>
2023-07-11 11:56:04 +0900

    [hotfix] FeignClient 및 Header 관련 설정 누락된 것 추가

--- msa-iamhere-post/src/main/java/com/personal/post/client/MemberApiClient.java
+++ msa-iamhere-post/src/main/java/com/personal/post/client/MemberApiClient.java
@@ -1,12 +1,17 @@
 package com.personal.post.client;
 
+import feign.Feign;
+import feign.Headers;
+import feign.RequestInterceptor;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @FeignClient(value = "MSA-IAMHERE-MEMBER")
+@Headers("Accept-Language: en-US")
 @Component
 public interface MemberApiClient {
     @GetMapping(value = "/get-current-member")
+    @Headers("Content-Type: application/json")
     public Long getCurrentMember();
 }
